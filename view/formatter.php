8938b5cce8051c9cac5e8ffaa1c1460be7095571
morido <morido@web.de>
2014-03-04 14:56:04 +0100

    Tiny refractoring to for relativeTime()

--- view/formatter.php
+++ view/formatter.php
@@ -41,38 +41,38 @@ class newsfeed_formatter implements formatter {
             echo "<a target=\"_blank\" href=\"".$post['link']."\">";
             echo htmlentities($post['text'], ENT_COMPAT | 'ENT_HTML5' | ENT_QUOTES, 'UTF-8');
             echo " ";
-            echo "<span class=\"datum\">".relativeTime($post['timestamp'])."</span>";
+            echo "<span class=\"datum\">".$this->relativeTime($post['timestamp'])."</span>";
             echo "</a>";
             echo "</li>";
             echo "\n";
         }
         echo "</ul>";
     }
-}
 
-/**
- * A function to turn an absolute timelength into a relative human-readable (German) one based on the current time
- *
- * @param integer $absoluteTimestamp Absolute input in seconds
- * @return string Relative time as human-readable string in German
- */
-function relativeTime($absoluteTimestamp) {
-    //this requires PHP 5.3
+    /**
+     * A function to turn an absolute timelength into a relative human-readable (German) one based on the current time
+     *
+     * @param integer $absoluteTimestamp Absolute input in seconds
+     * @return string Relative time as human-readable string in German
+     */
+    private function relativeTime($absoluteTimestamp) {
+        //this requires PHP 5.3
 
-    $now = new \DateTime();
-    $othertime = new \DateTime("@".$absoluteTimestamp);
-    $interval = $othertime->diff($now);
-    $days = $interval->days;
+        $now = new \DateTime();
+        $othertime = new \DateTime("@".$absoluteTimestamp);
+        $interval = $othertime->diff($now);
+        $days = $interval->days;
 
-    switch($days) {
-        case 0:
-            $output = "heute"; break;
-        case 1:
-            $output = "gestern"; break;
-        default;
-            $output = "vor ".$days." Tagen";
+        switch($days) {
+            case 0:
+                $output = "heute"; break;
+            case 1:
+                $output = "gestern"; break;
+            default;
+                $output = "vor ".$days." Tagen";
+        }
+        return $output;
     }
-    return $output;
 }
 
 
